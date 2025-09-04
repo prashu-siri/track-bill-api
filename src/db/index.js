434564725ab3a8954a8ab256dfe9705cda94bc12
Prashanth Kumar <prashusiri.1990@gmail.com>
2025-09-04 22:40:00 +0530

    SSL

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT || 5432,
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT || 5432,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 pool.on('error', (err) => {
