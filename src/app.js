abd6132b4e3fcc17bc3dd403c0b3de45972b6904
Prashanth Kumar <prashusiri.1990@gmail.com>
2025-09-04 22:06:06 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const billRoutes = require("./routes/billRoutes");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(billRoutes);
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
