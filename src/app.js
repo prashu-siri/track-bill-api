9b854e898b64cd0e93fea4faa7b36d9903b4b797
Prashanth Kumar <prashusiri.1990@gmail.com>
2025-09-04 22:21:06 +0530

    CORS changes

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ const cors = require("cors");
 
 const app = express();
 
+const corsOptions = {
+	origin: "https://track-bill-ui.onrender.com",
+	optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(billRoutes);
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
