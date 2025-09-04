8cd773862b40a94e2dfca690217de39940d5a0b5
Prashanth Kumar <prashusiri.1990@gmail.com>
2025-09-04 23:28:17 +0530

    Added routes

--- src/routes/billRoutes.js
+++ src/routes/billRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post("/api/bills", billController.createBill);
 router.get("/api/bills/:id", billController.getBill);
 router.get("/api/bills", billController.getAllBills);
+router.put("/api/bills/:id", billController.updateBill);
+router.delete("/api/bills/:id", billController.deleteBill);
 
 module.exports = router;
